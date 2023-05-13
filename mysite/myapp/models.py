81a3b31b327ba5fc500a5ba2334b25d5453e4ca7
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-05-13 13:55:29 +0300

    added image in models

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.CharField(max_length=200)
+    image = models.ImageField(blank=True, upload_to='images')
 
     def __str__(self):
         return self.name
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    #http://127.0.0.1:8000/hello/
     path("myapp/", include("myapp.urls", namespace="myapp"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
