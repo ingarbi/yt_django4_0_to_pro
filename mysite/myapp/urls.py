df57b8e1f625dda409511939e76a1ecf85d4a3ee
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-05-16 13:39:50 +0300

    update item

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from myapp.views import index, indexItem, add_item
+from myapp.views import index, indexItem, add_item, update_item
 
 app_name = "myapp"
 
 urlpatterns = [
     # http://127.0.0.1:8000/myapp/
-    path("", index),
+    path("", index, ),
     path("<int:my_id>/", indexItem, name="detail"),
     # http://127.0.0.1:8000/myapp/
     path("additem/", add_item, name="add_item"),
+    path("updateitem/<int:my_id>/", update_item, name="update_item"),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
 
@@ -24,3 +24,16 @@ def add_item(request):
         item = Product(name=name, price=price, description=description, image=image)
         item.save()
     return render(request, "myapp/additem.html")
+
+
+def update_item(request, my_id):
+    item = Product.objects.get(id=my_id)
+    if request.method == "POST":
+        item.name = request.POST.get("name")
+        item.price = request.POST.get("price")
+        item.description = request.POST.get("description")
+        item.image = request.FILES.get('upload', item.image)
+        item.save()
+        return redirect("/myapp/")
+    context = {"item": item}
+    return render(request, "myapp/updateitem.html", context) 
