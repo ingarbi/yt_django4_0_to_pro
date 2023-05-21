93191bdf0031663a6bba565a577ccb9f238beb0e
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-05-21 10:50:02 +0300

    #32 CBV

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from myapp.views import index, indexItem, add_item, update_item, delete_item
+from myapp.views import (
+    add_item,
+    update_item,
+    delete_item,
+    ProductListView,
+    ProductDetailView,
+)
 
 app_name = "myapp"
 
 urlpatterns = [
     # http://127.0.0.1:8000/myapp/
-    path("", index, name='index'),
-    path("<int:my_id>/", indexItem, name="detail"),
+    # path("", index, name='index'),
+    path("", ProductListView.as_view(), name="index"),
+    path("<int:pk>/", ProductDetailView.as_view(), name="detail"),
     # http://127.0.0.1:8000/myapp/
     path("additem/", add_item, name="add_item"),
     path("updateitem/<int:my_id>/", update_item, name="update_item"),
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,19 +2,30 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
 
 
-def index(request):
-    items = Product.objects.all()
-    context = {"items": items}
-    return render(request, "myapp/index.html", context)
+# def index(request):
+#     items = Product.objects.all()
+#     context = {"items": items}
+#     return render(request, "myapp/index.html", context)
 
 
-def indexItem(request, my_id):
-    item = Product.objects.get(id=my_id)
-    context = {"item": item}
-    return render(request, "myapp/detail.html", context=context)
+class ProductListView(ListView):
+    model = Product
+    template_name = "myapp/index.html"
+    context_object_name = "items"
 
+
+# def indexItem(request, my_id):
+#     item = Product.objects.get(id=my_id)
+#     context = {"item": item}
+#     return render(request, "myapp/detail.html", context=context)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "myapp/detail.html"
+    context_object_name = "item"
 
 @login_required
 def add_item(request):
