45ba29a3f3717a14cf6f42120f38a34f3df5640d
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-05-21 14:49:48 +0300

    #34 admin

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -3,6 +3,18 @@ from django.contrib import admin
 from .models import Product
 
 
-admin.site.register(Product)
+admin.site.site_header = "My Django App"
+admin.site.site_title = "Title of Django"
+admin.site.index_title = "My admin"
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "description")
+    search_fields = ("description",)
+    list_editable = ("price", "description")
+    actions = ('make_zero',)
+
+    def make_zero(self, request, queryset):
+        queryset.update(price=0)
+
+admin.site.register(Product, ProductAdmin)
